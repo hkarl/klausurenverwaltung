6c04107f1a3f0a9557af44ea2133d1cb156c7664
Holger Karl <hkarl@ieee.org>
2013-03-17 17:10:16 +0100

    admin ofrm nutzt select2 widgets in many-to-many filed!!

--- klausurensammlung/admin.py
+++ klausurensammlung/admin.py
@@ -1,5 +1,7 @@
 from klausurensammlung.models import Schlagwort, Stufe, MCFrage, StandardFrage
 from django.contrib import admin
+from django.db import models
+from django_select2.widgets import Select2MultipleWidget
 
 admin.site.register(Schlagwort)
 admin.site.register(Stufe)
@@ -10,8 +12,15 @@ class MCFrageAdmin (admin.ModelAdmin):
     ('Falsche Antworten', {'fields': ['falscheantwort1', 'falscheantwort2', 'falscheantwort3', 'falscheantwort4', 'falscheantwort5', ]}),
     
         ]
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': Select2MultipleWidget}
+    }
+
 
 class StandardFrageAdmin (admin.ModelAdmin):
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': Select2MultipleWidget}
+    }
     pass
 
 
